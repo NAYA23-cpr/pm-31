aa2e8f1f2b0d9c8b532cf7da7f90028a588fceb2
NAYA23-cpr <141000562+NAYA23-cpr@users.noreply.github.com>
2023-10-02 13:07:33 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,7 @@
+const login = (namaDepan, namaBelakang, email) => {
+    console.log("Login Screen")
+    console.log("Halo " + namaDepan + namaBelakang)
+    console.log("Email" + email)
+}
+
+login("Naya", " hinayatulloh", " nhinayatulloh@gamil.com")
---
+++ profile_reg_function.js
@@ -0,0 +1,7 @@
+function home(namaDepan, namaBelakang, email) {
+    console.log("Halo " + namaDepan + namaBelakang)
+    console.log("Email" + email)
+    console.log("Selamat Datang di Home Screen")
+}
+
+home("Naya", " hinayatulloh", " nhinayatulloh@gamil.com")
